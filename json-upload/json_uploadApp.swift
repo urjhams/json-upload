70fc5dac4bdc5e0e1abcef1b6be57a34003e9e07
Quân <quan@foolography.com>
2020-12-29 17:34:56 +0100

    Initial Commit

---
+++ json-upload/json_uploadApp.swift
@@ -0,0 +1,17 @@
+//
+//  json_uploadApp.swift
+//  json-upload
+//
+//  Created by Quân Đinh on 29.12.20.
+//
+
+import SwiftUI
+
+@main
+struct json_uploadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ json-uploadTests/json_uploadTests.swift
@@ -0,0 +1,33 @@
+//
+//  json_uploadTests.swift
+//  json-uploadTests
+//
+//  Created by Quân Đinh on 29.12.20.
+//
+
+import XCTest
+@testable import json_upload
+
+class json_uploadTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
