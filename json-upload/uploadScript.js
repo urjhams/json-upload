ce3b8f21ac54aa3b14e1939f459d593dcca54607
Quân <quan@foolography.com>
2020-12-30 14:50:50 +0100

    log json out

--- json-upload/uploadScript.js
+++ json-upload/uploadScript.js
@@ -13,17 +13,19 @@
 
  var json = require('./usb_conversion_tables.json');
 
- json.forEach(function (obj) {
-     db.collection("menu").add({
-         id: obj.id,
-         name: obj.name,
-         description: obj.description,
-         price: obj.price,
-         type: obj.type
-     }).then(function (docRef) {
-         console.log("Document written with ID: ", docRef.id);
-     })
-         .catch(function (error) {
-             console.error("Error adding document: ", error);
-         });
- });
+console.log(json);
+
+// json.forEach(function (obj) {
+//     db.collection("menu").add({
+//         id: obj.id,
+//         name: obj.name,
+//         description: obj.description,
+//         price: obj.price,
+//         type: obj.type
+//     }).then(function (docRef) {
+//         console.log("Document written with ID: ", docRef.id);
+//     })
+//         .catch(function (error) {
+//             console.error("Error adding document: ", error);
+//         });
+// });
