5a3a22e6e5d7b1cb2e289ddb96150c3d59a8f5d7
Quân <quan@foolography.com>
2020-12-30 20:25:10 +0100

    use json string

--- json-upload/uploadScript.js
+++ json-upload/uploadScript.js
@@ -1,19 +1,21 @@
- const firebase = require("firebase");
- // Required for side-effects
- require("firebase/firestore");
+const firebase = require("firebase");
+// Required for side-effects
+require("firebase/firestore");
 
- // Initialize Cloud Firestore through Firebase
- firebase.initializeApp({
-     apiKey: "AIzaSyDUEhb8jGrhNR53qEA0wB9f1M9IcnhPrXQ",
-     authDomain: "json-upload-12aa7.firebaseapp.com",
-     projectId: "json-upload-12aa7"
- });
+// Initialize Cloud Firestore through Firebase
+firebase.initializeApp({
+apiKey: "AIzaSyDUEhb8jGrhNR53qEA0wB9f1M9IcnhPrXQ",
+authDomain: "json-upload-12aa7.firebaseapp.com",
+projectId: "json-upload-12aa7"
+});
 
- var db = firebase.firestore();
+var db = firebase.firestore();
 
- var json = require('./usb_conversion_tables.json');
+var json = require('./usb_conversion_tables.json');
 
-console.log(json);
+var jsonString = JSON.parse(json);
+
+console.log(jsonString);
 
 // json.forEach(function (obj) {
 //     db.collection("menu").add({
