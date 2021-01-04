2916ffe82443399af928f3910a7386ec06e4d171
Quân <quan@foolography.com>
2021-01-04 18:36:14 +0100

    reverts script

--- json-upload/uploadScript.js
+++ json-upload/uploadScript.js
@@ -1,30 +1,30 @@
-//const firebase = require("firebase");
+const firebase = require("firebase");
 // Required for side-effects
-//require("firebase/storage");
+require("firebase/storage");
 
-//// Initialize Cloud Firestore through Firebase
-//firebase.initializeApp({
-//apiKey: "AIzaSyDUEhb8jGrhNR53qEA0wB9f1M9IcnhPrXQ",
-//authDomain: "json-upload-12aa7.firebaseapp.com",
-//projectId: "json-upload-12aa7"
-//});
+// Initialize Cloud Firestore through Firebase
+firebase.initializeApp({
+apiKey: "AIzaSyDUEhb8jGrhNR53qEA0wB9f1M9IcnhPrXQ",
+authDomain: "json-upload-12aa7.firebaseapp.com",
+projectId: "json-upload-12aa7"
+});
 
-//var db = firebase.firestore();
-//
-//var json = require('./usb_conversion_tables.json');
-//
-//var jsonString = JSON.stringify(json);
-//
-//db.collection("firmware").doc("current").set({
-//    json: jsonString,
-//    time: new Date().getTime()
-//})
-//.then(function() {
-//    console.log("Document successfully written!");
-//})
-//.catch(function(error) {
-//    console.error("Error adding document: ", error);
-//});
+var db = firebase.firestore();
+
+var json = require('./usb_conversion_tables.json');
+
+var jsonString = JSON.stringify(json);
+
+db.collection("firmware").doc("current").set({
+    json: jsonString,
+    time: new Date().getTime()
+})
+.then(function() {
+    console.log("Document successfully written!");
+})
+.catch(function(error) {
+    console.error("Error adding document: ", error);
+});
 
 //var firebaseConfig = {
 //    apiKey: 'AIzaSyDUEhb8jGrhNR53qEA0wB9f1M9IcnhPrXQ',
@@ -47,38 +47,38 @@
 //    console.log("uploaded a file");
 //});
 
-const {Storage} = require('@google-cloud/storage');
-const express = require("express");
-
-const app = new express();
-
-
-const storage = new Storage({
-    keyFilename: 'AIzaSyDUEhb8jGrhNR53qEA0wB9f1M9IcnhPrXQ',
- });
-
-let bucketName = 'gs://json-upload-12aa7.appspot.com'
-
-let filename = './json-upload/test.zip';
-
-// Testing out upload of file
-const uploadFile = async() => {
-
-    // Uploads a local file to the bucket
-    await storage.bucket(bucketName).upload(filename, {
-        // Support for HTTP requests made with `Accept-Encoding: gzip`
-        gzip: true,
-        // By setting the option `destination`, you can change the name of the
-        // object you are uploading to a bucket.
-        metadata: {
-            // Enable long-lived HTTP caching headers
-            // Use only if the contents of the file will never change
-            // (If the contents will change, use cacheControl: 'no-cache')
-            cacheControl: 'public, max-age=31536000',
-        },
-});
-
-console.log(`${filename} uploaded to ${bucketName}.`);
-}
-
-uploadFile();
+//const {Storage} = require('@google-cloud/storage');
+//const express = require("express");
+//
+//const app = new express();
+//
+//
+//const storage = new Storage({
+//    keyFilename: 'AIzaSyDUEhb8jGrhNR53qEA0wB9f1M9IcnhPrXQ',
+// });
+//
+//let bucketName = 'gs://json-upload-12aa7.appspot.com'
+//
+//let filename = './json-upload/test.zip';
+//
+//// Testing out upload of file
+//const uploadFile = async() => {
+//
+//    // Uploads a local file to the bucket
+//    await storage.bucket(bucketName).upload(filename, {
+//        // Support for HTTP requests made with `Accept-Encoding: gzip`
+//        gzip: true,
+//        // By setting the option `destination`, you can change the name of the
+//        // object you are uploading to a bucket.
+//        metadata: {
+//            // Enable long-lived HTTP caching headers
+//            // Use only if the contents of the file will never change
+//            // (If the contents will change, use cacheControl: 'no-cache')
+//            cacheControl: 'public, max-age=31536000',
+//        },
+//});
+//
+//console.log(`${filename} uploaded to ${bucketName}.`);
+//}
+//
+//uploadFile();
