0fcc96525a589194c72989d346a289f6f4926647
Quân <quan@foolography.com>
2021-01-07 13:15:42 +0100

    use os filePath

--- json-upload/uploadScript.py
+++ json-upload/uploadScript.py
@@ -2,8 +2,11 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import storage
 import pathlib
+import os
 
-cred = credentials.Certificate('./json-upload-301011-6cd8a2247084.json')
+dirname = os.path.dirname(__file__)
+keyFilePath = os.path.join(dirname, './json-upload-301011-6cd8a2247084.json')
+cred = credentials.Certificate(keyFilePath)
 firebase_admin.initialize_app(cred, {
     'storageBucket' : 'json-upload-12aa7.appspot.com'
 })
