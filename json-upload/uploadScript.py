d0b15fe5c7b4346528d55826dc9fc84a4ab85fa0
Quân <quan@foolography.com>
2021-01-07 18:55:06 +0100

    change script file

--- json-upload/uploadScript.py
+++ json-upload/uploadScript.py
@@ -1,7 +1,9 @@
 import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import storage
+from firebase_admin import credentials, storage
 import os
+
+# Import UUID4 to create token
+from uuid import uuid4
 
 dirname = os.path.dirname(os.path.realpath(__file__))
 keyFilePath = dirname + '/json-upload-12aa7-12e10899219f.json'
@@ -17,8 +19,17 @@ fileName = dirname + '/test.zip'
 
 blob = bucket.blob(fileName)
 
+# Create new token
+accessToken = uuid4()
+
+# Create new dictionary with the metadata
+metadata = { "firebaseStorageDownloadTokens": accessToken }
+
+# Set meta data for the blob wich contains the access token
+blob.metadata = metadata
+
 blob.upload_from_filename(fileName)
 
-blob.make_public()
+#blob.make_public()
 
 print("your file url ", blob.public_url)
