53779a419daa6ab0093aa766542c110006e8d967
Quân <quan@foolography.com>
2021-01-08 11:44:16 +0100

    update script with comments

--- json-upload/uploadScript.py
+++ json-upload/uploadScript.py
@@ -5,9 +5,11 @@ import os
 # Import UUID4 to create token
 from uuid import uuid4
 
+# get google cloud's service account key
 dirname = os.path.dirname(os.path.realpath(__file__))
 keyFilePath = dirname + '/json-upload-12aa7-12e10899219f.json'
 
+# apply the bucket domain to the credentials
 cred = credentials.Certificate(keyFilePath)
 firebase_admin.initialize_app(cred, {
     'storageBucket' : 'json-upload-12aa7.appspot.com'
@@ -19,6 +21,7 @@ fileName = 'test.zip'
 
 fileFullPath = dirname + '/' + fileName
 
+# if the file name contains file path, the bucket will create folders corresponding to the path.
 blob = bucket.blob(fileName)
 
 # Create new token, this one only used for downloading directly from firebase console page
@@ -30,8 +33,10 @@ metadata = { "firebaseStorageDownloadTokens": accessToken }
 # Set meta data for the blob wich contains the access token
 blob.metadata = metadata
 
+#upload to firebase storage
 blob.upload_from_filename(fileFullPath)
 
+# optional
 blob.make_public()
 
 print("your file url ", blob.public_url)
