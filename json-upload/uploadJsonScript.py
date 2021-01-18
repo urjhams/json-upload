3065fbf3f106fee24c82cb71946acea2bb0a6326
Quân Đinh <urjhams@gmail.com>
2021-01-18 22:51:03 +0100

    Update uploadJsonScript.py

--- json-upload/uploadJsonScript.py
+++ json-upload/uploadJsonScript.py
@@ -2,6 +2,9 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 import os
 import json
+import sys
+
+print(sys.argv[1])
 
 # base64 decoded key file will be stored in temporary directory
 # https://github.com/marketplace/actions/base64-to-file
