839e8fb90a6d98849567056fb27c0e01e74a011d
Quân <quan@foolography.com>
2021-01-22 12:53:15 +0100

    update

--- json-upload/uploadScript.py
+++ json-upload/uploadScript.py
@@ -22,30 +22,33 @@ firebase_admin.initialize_app(cred, {
 # refer to the storage bucket
 bucket = storage.bucket()
 
-# get the upload file's path in repository's directory
-fileName = 'test.zip'
-dirname = os.path.dirname(os.path.realpath(__file__))
-fileFullPath = dirname + '/' + fileName
+def upload():
+    # get the upload file's path in repository's directory
+    fileName = 'test.zip'
+    dirname = os.path.dirname(os.path.realpath(__file__))
+    fileFullPath = dirname + '/' + fileName
 
-# if the file name contains file path, the bucket will create folders corresponding to the path.
-blob = bucket.blob(fileName)
+    # if the file name contains file path, the bucket will create folders corresponding to the path.
+    blob = bucket.blob(fileName)
 
-if blob.exists():
-    sys.exit("file existed")
+    if blob.exists():
+        sys.exit("file existed")
 
-# Create new token, this one only used for downloading directly from firebase console page
-accessToken = uuid4()
+    # Create new token, this one only used for downloading directly from firebase console page
+    accessToken = uuid4()
 
-# Create new dictionary with the metadata
-metadata = { "firebaseStorageDownloadTokens": accessToken }
+    # Create new dictionary with the metadata
+    metadata = { "firebaseStorageDownloadTokens": accessToken }
 
-# Set meta data for the blob wich contains the access token
-blob.metadata = metadata
+    # Set meta data for the blob wich contains the access token
+    blob.metadata = metadata
 
-#upload to firebase storage
-blob.upload_from_filename(fileFullPath)
+    #upload to firebase storage
+    blob.upload_from_filename(fileFullPath)
 
-# optional
-blob.make_public()
+    # optional
+    blob.make_public()
 
-print("your file url ", blob.public_url)
+    print("your file url ", blob.public_url)
+
+upload()
