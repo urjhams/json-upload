3d385cdd5cd876fd3fd4a8290bd3705c0b3d367b
Quân Đinh <urjhams@gmail.com>
2021-11-02 14:03:15 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 import sys
-
-sys.exit("forced script run fail test by change this string's content to anything else :wink: :wink:")
+print("print something")
+# sys.exit("forced script run fail test by change this string's content to anything else :wink: :wink:")
